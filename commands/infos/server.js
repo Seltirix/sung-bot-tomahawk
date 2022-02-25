9de66c7d156e769f766e19bbf69df3c3000141fd
Seltirix <63235219+Seltirix@users.noreply.github.com>
2022-02-25 11:01:21 +0100

    Delete commands/infos directory

--- commands/infos/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'server',
-  description: 'Server informations.',
-
-  execute(client, message) {
-    message.channel.send(`Nom du serveur : ${message.guild.name}\n Nombre d'utilisateurs : ${message.guild.memberCount}`);
-  }
-};
